9d6dbbe25c59e9e6614275ea9b2aaa3c19490bb1
Javier Matusevich <javiermatusevich@gmail.com>
2015-10-09 17:24:29 -0300

    some copy, statistics

--- client/templates/home/home.js
+++ client/templates/home/home.js
@@ -2,5 +2,21 @@ Template.home.helpers({
     reports: function()
     {
         return Reports.find({});
+    },
+    countReports: function ()
+    {
+        return Reports.find({}).count();
+    },
+    countAggressors: function ()
+    {
+        return Aggressors.find({}).count();
+    },
+    countVictims: function ()
+    {
+        return Meteor.users.find({}).count();
+    },
+    isPlural: function (number)
+    {
+        return number!=0 && number>1;
     }
 });
