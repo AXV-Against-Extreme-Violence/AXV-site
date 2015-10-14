a218239a27683655b1838c110488a1d13c1f34ed
Javier Matusevich <javiermatusevich@gmail.com>
2015-10-14 20:35:42 -0300

    fixed not dimised menu in mobile

--- client/templates/includes/header.js
+++ client/templates/includes/header.js
@@ -9,3 +9,11 @@ Template.header.events({
        }
    }
 });
+
+$(document).on('click','.navbar-collapse.in',function(e) {
+
+    if( $(e.target).is('a') && ( $(e.target).attr('class') != 'dropdown-toggle' ) ) {
+        $(this).collapse('hide');
+    }
+
+});
